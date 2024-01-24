57711a92f43b4e5ad32997aae99d2189cbead654
Taalaibekova-Arslana <arslanataalajbekova@gmail.com>
2024-01-24 12:01:31 +0600

    added render

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-	const [userId, setUserId] = useState(1);
+	const [posts, setPosts] = useState([]);
 
 	useEffect(() => {
 		getTodos();
-	}, [userId]);
+	}, []);
 
 	const getTodos = async () => {
 		try {
 			const response = await fetch(
-				`https://jsonplaceholder.typicode.com/todos/${userId}`
+				`https://jsonplaceholder.typicode.com/posts`
 			);
 			const responseData = await response.json();
-			console.log(responseData);
+			setPosts(responseData);
 		} catch (e) {
 			console.error("Backend upal", e);
 		}
 	};
 	return (
 		<div>
-			<input
-				type="number"
-				value={userId}
-				onChange={(e) => setUserId(e.target.value)}
-			/>
-			{/* <button onClick={getTodos}>getTodos</button> */}
+			{posts.map((item) => (
+				<div key={item.id}>
+					<h1>{item.name}</h1>
+					<p>{item.body}</p>
+				</div>
+			))}
 		</div>
 	);
 };
